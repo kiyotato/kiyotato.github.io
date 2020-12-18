9171ba7f1a189e78296df157d4d9c0dc438b2963
kiyotato <kiyotato@users.noreply.github.com>
2020-12-19 01:02:35 +0200

    com

--- mysite/myapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('<int:user_id>', views.user_id, name='user_id'),
-    path("register", views.register_request, name="register"),
-]
